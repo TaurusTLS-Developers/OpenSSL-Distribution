636a1ac5ec85c784b21882e573f81517e79c7e2a
J. Peter Mugaas <jpmugaas@suddenlink.net>
2025-12-14 16:31:58 -0500

    Fix errant space character and build OpenSSL 3.6.0.

--- build-scripts/build-win32.cmd
+++ build-scripts/build-win32.cmd
@@ -1,7 +1,7 @@
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 mkdir win32-hybrid
 cd win32-hybrid
-set list="3.5.4" "3.4.3" "3.3.5 " "3.2.6" "3.0.18" 
+set list="3.6.0" "3.5.4" "3.4.3" "3.3.5" "3.2.6" "3.0.18" 
 SET CL=/D_WIN32_WINNT=0x0601 /D_WIN32_IE=0x0900 %CL%
 SET LINK=/SUBSYSTEM:CONSOLE,6.01 %LINK%
 (for %%a in (%list%) do (
