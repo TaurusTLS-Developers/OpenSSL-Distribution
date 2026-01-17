9900c86d1eb0fda61e6e5d547286e3e6b40a1cc5
J. Peter Mugaas <jpmugaas@suddenlink.net>
2026-01-17 04:22:27 -0500

    OpenSSL 3.0.18 - build for arm64.

--- build-scripts/build-win64-arm64.cmd
+++ build-scripts/build-win64-arm64.cmd
@@ -1,7 +1,7 @@
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 mkdir win64-arm
 cd win64-arm
-set list=3.6.0 3.5.4 3.4.3 3.3.5 3.2.6 
+set list=3.6.0 3.5.4 3.4.3 3.3.5 3.2.6 3.0.18
 SET CL=/D_WIN32_WINNT=0x0601 /D_WIN32_IE=0x0900 %CL%
 SET LINK=/SUBSYSTEM:CONSOLE,6.01 %LINK%
 for %%a in (%list%) do (
